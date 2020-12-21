99445f8cfb84974aff327e6c64abc59895489152
James Hilliard <james.hilliard1@gmail.com>
2020-12-20 23:58:57 -0700

    Fix vertx support.

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const Router = (typeof vertex === 'undefined' ) ? require("./lib/router") : require("./lib/vertex-router");
+const Router = (typeof vertx !== 'undefined' && vertx !== null) ? require("./lib/vertx-router") : require("./lib/router");
 module.exports = Router;
